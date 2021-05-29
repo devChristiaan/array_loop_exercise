a0ce8331a8edd9604a5beb03699a890b22212e03
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-29 18:36:02 -0400

    fixed sort function bug. Not creating li elements correctly

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ $(function() {
 
     // Take the fruit array as an argument
     function sort (fruit) {
-      const newFruitArray = [...fruit]
+      let newFruitArray = [...fruit]
       // Sort the fruit alphabetically (don't forget to make a copy of the array so you don't modify the original. Look up the array spread operator)
+      // Map over each fruit and append a list item with the name of a fruit inside of it
       
       newFruitArray.sort()
 
-      // Map over each fruit and append a list item with the name of a fruit inside of it
-      newFruitArray.map(fruit =>{
-        `<li>${fruit}</li>`
+      let htmlElements = []
+      newFruitArray.forEach((fruit) =>{
+        htmlElements.push("<li class='fruitItem'>" + fruit + "</li>")
       })
-      return newFruitArray
+
+      console.log(htmlElements);
+      
+      return htmlElements
     }
 
     // Finally use jQuery to dump your html into the unordered list above
